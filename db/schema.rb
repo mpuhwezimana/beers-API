d977996517bbd71779f3d621817156bfe91f5c4a
mpuhwezimana <mpuhwearsene.com>
2017-02-20 12:24:18 +0200

    work

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170214120501) do
+ActiveRecord::Schema.define(version: 20170220090141) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 20170214120501) do
     t.string   "ibu"
     t.string   "alcohol"
     t.string   "blg"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "api_key"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
