0b82014b6d3f51833e5012915cb511cc18c4fd19
mpuhwezimana <mpuhwearsene.com>
2017-03-02 11:42:32 +0200

    installed angular

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/", to: 'beers#index'
+  get "/", to: 'ctrlbeers#index'
   get "/beers", to: 'beers#index'
 
   namespace :api do
